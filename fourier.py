e37ee9a076d8226320a961ee605f27a4a51ef5d1
Jay Krishna <jayk674@gmail.com>
2017-06-14 18:46:21 +0530

    Updated Fourier

--- fourier.py
+++ fourier.py
@@ -1,12 +1,13 @@
 import numpy as np
-from scipy.fftpack import fft,fftshift
+from scipy.fftpack import fft, fftshift
 
-def CalcFourier(signal,fs,fc):
 
-	step=1/fs
-	pole=fs
+def CalcFourier(signal, fs, fc):
 
-	transform=20*np.log10(np.absolute(fftshift(fft(signal))))
-	N=transform.shape[0]
-	frequency=np.arange((-1*pole)/2+fc,pole/2+fc,fs/N)
-	return frequency,transform
+    step = 1 / fs
+    pole = fs
+
+    transform = 20 * np.log10(np.absolute(fftshift(fft(signal))))
+    N = transform.shape[0]
+    frequency = np.arange((-1 * pole) / 2 + fc, pole / 2 + fc, fs / N)
+    return frequency, transform
