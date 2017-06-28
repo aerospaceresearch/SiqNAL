e8483d33d640cf5a2ff75d7a63bc5839cfcb99ad
Jay Krishna <jayk674@gmail.com>
2017-06-28 21:57:21 +0530

    Added till bandpass filter

--- Screens/analysisscreen.py
+++ Screens/analysisscreen.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'analysisscreen.ui'
+# Form implementation generated from reading ui file '/home/matrix/Desktop/siqnal/qt/analysisscreen.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
@@ -16,21 +16,25 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
+
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.setEnabled(True)
-        MainWindow.resize(558, 500)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Maximum)
+        MainWindow.resize(869, 535)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMinimumSize(QtCore.QSize(0, 30))
         self.centralwidget = QtGui.QWidget(MainWindow)
@@ -42,10 +46,12 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3 = QtGui.QHBoxLayout()
         self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
         self.LogoDisplay = QtGui.QLabel(self.centralwidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.LogoDisplay.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.LogoDisplay.sizePolicy().hasHeightForWidth())
         self.LogoDisplay.setSizePolicy(sizePolicy)
         self.LogoDisplay.setText(_fromUtf8(""))
         self.LogoDisplay.setAlignment(QtCore.Qt.AlignCenter)
@@ -143,10 +149,12 @@ class Ui_MainWindow(object):
         self.verticalLayout_3.addWidget(self.line_3)
         self.MainTab = QtGui.QTabWidget(self.centralwidget)
         self.MainTab.setEnabled(False)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.MainTab.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.MainTab.sizePolicy().hasHeightForWidth())
         self.MainTab.setSizePolicy(sizePolicy)
         self.MainTab.setMinimumSize(QtCore.QSize(0, 0))
         self.MainTab.setObjectName(_fromUtf8("MainTab"))
@@ -157,10 +165,12 @@ class Ui_MainWindow(object):
         self.verticalLayout_4 = QtGui.QVBoxLayout()
         self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
         self.FileLogoDisplay = QtGui.QLabel(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.FileLogoDisplay.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.FileLogoDisplay.sizePolicy().hasHeightForWidth())
         self.FileLogoDisplay.setSizePolicy(sizePolicy)
         self.FileLogoDisplay.setMinimumSize(QtCore.QSize(0, 40))
         font = QtGui.QFont()
@@ -187,19 +197,23 @@ class Ui_MainWindow(object):
         self.horizontalLayout_6 = QtGui.QHBoxLayout()
         self.horizontalLayout_6.setObjectName(_fromUtf8("horizontalLayout_6"))
         self.SampleFreqLabel = QtGui.QLabel(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Preferred)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.SampleFreqLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.SampleFreqLabel.sizePolicy().hasHeightForWidth())
         self.SampleFreqLabel.setSizePolicy(sizePolicy)
         self.SampleFreqLabel.setMinimumSize(QtCore.QSize(138, 0))
         self.SampleFreqLabel.setObjectName(_fromUtf8("SampleFreqLabel"))
         self.horizontalLayout_6.addWidget(self.SampleFreqLabel)
         self.SampleFreqInput = QtGui.QLineEdit(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.SampleFreqInput.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.SampleFreqInput.sizePolicy().hasHeightForWidth())
         self.SampleFreqInput.setSizePolicy(sizePolicy)
         self.SampleFreqInput.setObjectName(_fromUtf8("SampleFreqInput"))
         self.horizontalLayout_6.addWidget(self.SampleFreqInput)
@@ -207,19 +221,23 @@ class Ui_MainWindow(object):
         self.horizontalLayout_7 = QtGui.QHBoxLayout()
         self.horizontalLayout_7.setObjectName(_fromUtf8("horizontalLayout_7"))
         self.CentreFreqLabel = QtGui.QLabel(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Preferred)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.CentreFreqLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.CentreFreqLabel.sizePolicy().hasHeightForWidth())
         self.CentreFreqLabel.setSizePolicy(sizePolicy)
         self.CentreFreqLabel.setMinimumSize(QtCore.QSize(149, 0))
         self.CentreFreqLabel.setObjectName(_fromUtf8("CentreFreqLabel"))
         self.horizontalLayout_7.addWidget(self.CentreFreqLabel)
         self.CentreFreqInput = QtGui.QLineEdit(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.CentreFreqInput.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.CentreFreqInput.sizePolicy().hasHeightForWidth())
         self.CentreFreqInput.setSizePolicy(sizePolicy)
         self.CentreFreqInput.setObjectName(_fromUtf8("CentreFreqInput"))
         self.horizontalLayout_7.addWidget(self.CentreFreqInput)
@@ -234,13 +252,16 @@ class Ui_MainWindow(object):
         self.line.setObjectName(_fromUtf8("line"))
         self.horizontalLayout_8.addWidget(self.line)
         self.verticalLayout_5 = QtGui.QVBoxLayout()
-        self.verticalLayout_5.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
+        self.verticalLayout_5.setSizeConstraint(
+            QtGui.QLayout.SetDefaultConstraint)
         self.verticalLayout_5.setObjectName(_fromUtf8("verticalLayout_5"))
         self.label_5 = QtGui.QLabel(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.label_5.sizePolicy().hasHeightForWidth())
         self.label_5.setSizePolicy(sizePolicy)
         self.label_5.setMinimumSize(QtCore.QSize(0, 40))
         font = QtGui.QFont()
@@ -257,24 +278,30 @@ class Ui_MainWindow(object):
         self.TimeLaunchButton.setObjectName(_fromUtf8("TimeLaunchButton"))
         self.verticalLayout_5.addWidget(self.TimeLaunchButton)
         self.FourierLaunchButton = QtGui.QPushButton(self.Signal1)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.FourierLaunchButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.FourierLaunchButton.sizePolicy().hasHeightForWidth())
         self.FourierLaunchButton.setSizePolicy(sizePolicy)
-        self.FourierLaunchButton.setObjectName(_fromUtf8("FourierLaunchButton"))
+        self.FourierLaunchButton.setObjectName(
+            _fromUtf8("FourierLaunchButton"))
         self.verticalLayout_5.addWidget(self.FourierLaunchButton)
         self.PowerLaunchButton = QtGui.QPushButton(self.Signal1)
         self.PowerLaunchButton.setObjectName(_fromUtf8("PowerLaunchButton"))
         self.verticalLayout_5.addWidget(self.PowerLaunchButton)
         self.SpectrogramLaunchButton = QtGui.QPushButton(self.Signal1)
-        self.SpectrogramLaunchButton.setObjectName(_fromUtf8("SpectrogramLaunchButton"))
+        self.SpectrogramLaunchButton.setObjectName(
+            _fromUtf8("SpectrogramLaunchButton"))
         self.verticalLayout_5.addWidget(self.SpectrogramLaunchButton)
         self.Spectrogram2LaunchButton = QtGui.QPushButton(self.Signal1)
-        self.Spectrogram2LaunchButton.setObjectName(_fromUtf8("Spectrogram2LaunchButton"))
+        self.Spectrogram2LaunchButton.setObjectName(
+            _fromUtf8("Spectrogram2LaunchButton"))
         self.verticalLayout_5.addWidget(self.Spectrogram2LaunchButton)
         self.BandpassLaunchButton = QtGui.QPushButton(self.Signal1)
-        self.BandpassLaunchButton.setObjectName(_fromUtf8("BandpassLaunchButton"))
+        self.BandpassLaunchButton.setObjectName(
+            _fromUtf8("BandpassLaunchButton"))
         self.verticalLayout_5.addWidget(self.BandpassLaunchButton)
         self.horizontalLayout_8.addLayout(self.verticalLayout_5)
         self.MainTab.addTab(self.Signal1, _fromUtf8(""))
@@ -295,22 +322,39 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.ProgramNameDisplay.setText(_translate("MainWindow", "Google Summer of Code 2017", None))
-        self.ProjectNameDIsplay.setText(_translate("MainWindow", "Project Name: SIQNAL", None))
-        self.CodeSourceDisplay.setText(_translate("MainWindow", "Code: <a href=\"https://github.com/aerospaceresearch/siqnal\">Github</a>", None))
-        self.MentorNameDisplay.setText(_translate("MainWindow", "Mentor: Andreas Horning", None))
-        self.AuthorNameDisplay.setText(_translate("MainWindow", "Author: Jay Krishna", None))
-        self.FileSelectButton.setText(_translate("MainWindow", "Select File", None))
-        self.SampleFreqLabel.setText(_translate("MainWindow", "Sampling Frequency(MHz)", None))
-        self.CentreFreqLabel.setText(_translate("MainWindow", "Centre Frequency(MHz)", None))
-        self.ImportValueButton.setText(_translate("MainWindow", "Import", None))
-        self.label_5.setText(_translate("MainWindow", "Visualization Section", None))
-        self.TimeLaunchButton.setText(_translate("MainWindow", "Time Domain Analysis", None))
-        self.FourierLaunchButton.setText(_translate("MainWindow", "Fourier Analysis", None))
-        self.PowerLaunchButton.setText(_translate("MainWindow", "Power Spectral Analysis", None))
-        self.SpectrogramLaunchButton.setText(_translate("MainWindow", "Spectrogram Analysis", None))
-        self.Spectrogram2LaunchButton.setText(_translate("MainWindow", "Spectrogram Test", None))
-        self.BandpassLaunchButton.setText(_translate("MainWindow", "Bandpass Filter", None))
-        self.MainTab.setTabText(self.MainTab.indexOf(self.Signal1), _translate("MainWindow", "Signal 1", None))
-        self.MainTab.setTabText(self.MainTab.indexOf(self.tab_3), _translate("MainWindow", "Analysis", None))
-
+        self.ProgramNameDisplay.setText(_translate(
+            "MainWindow", "Google Summer of Code 2017", None))
+        self.ProjectNameDIsplay.setText(_translate(
+            "MainWindow", "Project Name: SIQNAL", None))
+        self.CodeSourceDisplay.setText(_translate(
+            "MainWindow", "Code: <a href=\"https://github.com/aerospaceresearch/siqnal\">Github</a>", None))
+        self.MentorNameDisplay.setText(_translate(
+            "MainWindow", "Mentor: Andreas Horning", None))
+        self.AuthorNameDisplay.setText(_translate(
+            "MainWindow", "Author: Jay Krishna", None))
+        self.FileSelectButton.setText(
+            _translate("MainWindow", "Select File", None))
+        self.SampleFreqLabel.setText(_translate(
+            "MainWindow", "Sampling Frequency(MHz)", None))
+        self.CentreFreqLabel.setText(_translate(
+            "MainWindow", "Centre Frequency(MHz)", None))
+        self.ImportValueButton.setText(
+            _translate("MainWindow", "Import", None))
+        self.label_5.setText(_translate(
+            "MainWindow", "Visualization Section", None))
+        self.TimeLaunchButton.setText(_translate(
+            "MainWindow", "Time Domain Analysis", None))
+        self.FourierLaunchButton.setText(_translate(
+            "MainWindow", "Fourier Analysis", None))
+        self.PowerLaunchButton.setText(_translate(
+            "MainWindow", "Power Spectral Analysis", None))
+        self.SpectrogramLaunchButton.setText(_translate(
+            "MainWindow", "Spectrogram Analysis", None))
+        self.Spectrogram2LaunchButton.setText(
+            _translate("MainWindow", "Spectrogram Test", None))
+        self.BandpassLaunchButton.setText(
+            _translate("MainWindow", "Bandpass Filter", None))
+        self.MainTab.setTabText(self.MainTab.indexOf(
+            self.Signal1), _translate("MainWindow", "Signal 1", None))
+        self.MainTab.setTabText(self.MainTab.indexOf(
+            self.tab_3), _translate("MainWindow", "Analysis", None))
