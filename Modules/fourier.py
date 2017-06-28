06749ead1245b8facbf7846167275a155e5b7f29
Jay Krishna <jayk674@gmail.com>
2017-06-28 21:53:44 +0530

    Added till bandpass filter

--- Modules/fourier.py
+++ Modules/fourier.py
@@ -7,7 +7,7 @@ def CalcFourier(signal, fs, fc):
     step = 1 / fs
     pole = fs
 
-    transform = 20 * np.log10(np.absolute(fftshift(fft(signal))/len(signal)))
+    transform = 20 * np.log10(np.absolute(fftshift(fft(signal)) / len(signal)))
     N = transform.shape[0]
     frequency = np.arange((-1 * pole) / 2 + fc, pole / 2 + fc, fs / N)
     return frequency, transform
