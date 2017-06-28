d48cd55971ecd8b9b16a82cb3716346dc86782d8
Jay Krishna <jayk674@gmail.com>
2017-06-28 21:53:23 +0530

    Added till bandpass filter

--- Modules/checkmodules.py
+++ Modules/checkmodules.py
@@ -1,11 +1,13 @@
 import importlib
 import sys
 
+
 def check_version():
-    if(sys.version_info <(3,4,0)):
-        return (False,"You need Python 3.4 or later to run this project")
+    if(sys.version_info < (3, 4, 0)):
+        return (False, "You need Python 3.4 or later to run this project")
     else:
-        return (True,"Python minimum version requirement satisfied")
+        return (True, "Python minimum version requirement satisfied")
+
 
 def check_lib(package_name, package_version):
 
@@ -15,14 +17,15 @@ def check_lib(package_name, package_version):
         minimum_version = package_version.rstrip()
         if(not(installed_version >= minimum_version)):
             raise Exception("Module name: {} installed but Minimum version: {} required. Found version: {}".format(
-                package_name, minimum_version,installed_version))
+                package_name, minimum_version, installed_version))
     except ImportError:
-        return (False,"Module name: {} not installed !!".format(package_name))
+        return (False, "Module name: {} not installed !!".format(package_name))
     except Exception as error:
-        return (False,error.args[0])
+        return (False, error.args[0])
     else:
-        return (True,"Module name: {} installed with minimum version requirement satisfied.".format(
+        return (True, "Module name: {} installed with minimum version requirement satisfied.".format(
             package_name))
+
 
 if __name__ == "__main__":
     check_modules()
