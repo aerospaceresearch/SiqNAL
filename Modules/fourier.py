19c5a4383229dc635e705b609a29d5971e2098dd
Jay Krishna <jayk674@gmail.com>
2017-07-05 21:25:17 +0530

    Added doc string

--- Modules/fourier.py
+++ Modules/fourier.py
@@ -1,9 +1,36 @@
+"""
+    **Author :** *Jay Krishna*
+
+    This module computes the length normalized fourier transform of the signal.
+
+"""
 import numpy as np
 from scipy.fftpack import fft, fftshift
 
 
 def CalcFourier(signal, fs, fc):
+    """
+        This function calculates the fourier transform of a signal. The fourier transform's
+        zero frequency is shifted to the centre of the spectrum. Further the fourier transform
+        is length normalized.
 
+        Parameters
+        -------------------------
+            signal : ndarray
+                Numpy complex array of shifted signal.
+            fs : float
+                Sampling frequency of the signal.
+            fc : float
+                Centre frequency of the signal.
+
+        Returns
+        -----------------------
+            frequency : ndarray
+                Numpy array of values of frequencies present in the signal.
+            transform : ndarray
+                Numpy array of computed fourier transform.
+
+    """
     step = 1 / fs
     pole = fs
 
