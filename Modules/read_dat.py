c8202f79da9e92a29f5608212e06a2d1a49316f7
Jay Krishna <jayk674@gmail.com>
2017-07-05 21:25:23 +0530

    Added doc string

--- Modules/read_dat.py
+++ Modules/read_dat.py
@@ -1,7 +1,29 @@
+"""
+    **Author :** *Jay Krishna*
+
+    This module read (.dat) file from physical storage as a memory-
+    mapped file.
+
+"""
+
 import numpy as np
 
 
 def loaddata(filename):
+    """
+        This function opens a (.dat) format file specified by it's absolute 
+        path and returns a memory-mapped file object.
+
+        Parameters
+        --------------------------
+            filename : string
+                Absolute path to (.dat) format file to be read.
+
+        Returns
+        --------------------------
+            signal : file object
+                Memory mapped file object of specified (.dat) file.
+    """
 
     try:
 
