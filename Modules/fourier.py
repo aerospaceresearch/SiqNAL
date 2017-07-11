1b7a5184e88223c083ae98502a72739204b04e62
Jay Krishna <jayk674@gmail.com>
2017-07-11 19:15:04 +0530

    Added waterfall

--- Modules/fourier.py
+++ Modules/fourier.py
@@ -2,19 +2,21 @@ import numpy as np
 from scipy.fftpack import fft, fftshift
 
 
-def CalcFourier(signal, fs, fc):
+def CalcFourier(signal, fs, fc, nfft=0):
 
     step = 1 / fs
     pole = fs
-
+    if nfft == 0:
+        nfft = len(signal)
     # One way I want to implement this
-    # transform=20*np.log10(np.absolute(fftshift(fft(signal))/len(signal)))
+    transform = 20 * \
+        np.log10(np.absolute(fftshift(fft(signal, n=nfft)) / len(signal)))
 
     # Other way I implemented keeping the log thing away
-    transform = (np.absolute(fftshift(fft(signal))))
+    #transform = (np.absolute(fftshift(fft(signal,n=nfft))))
 
-    N = transform.shape[0]
+    #N = transform.shape[0]
 
-    frequency = np.arange((-1 * pole) / 2 + fc, pole / 2 + fc, fs / N)
+    frequency = np.arange((-1 * pole) / 2 + fc, pole / 2 + fc, fs / nfft)
 
     return frequency, transform
