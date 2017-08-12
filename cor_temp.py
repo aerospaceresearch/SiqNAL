1ebb5f29f16311cf1785441483adab2b0815f36a
Jay Krishna <jayk674@gmail.com>
2017-08-12 23:57:06 +0530

    New cross-correlation

--- cor_temp.py
+++ cor_temp.py
@@ -10,7 +10,8 @@ from Modules import read_wav
 from Modules import SignalData
 from Modules import freqbands
 
-def find_segs(samples, threshold, min_dur, merge_dur,fs,n):
+
+def find_segs(samples, threshold, min_dur, merge_dur, fs, n):
     start = -1
     end = -1
     segments = []
@@ -30,7 +31,7 @@ def find_segs(samples, threshold, min_dur, merge_dur,fs,n):
             if(dur > min_dur):
                 if(len(segments) == 0):
                     segments.append([start, end, dur])
-                    points.append([int(start*n), int(end*n)])
+                    points.append([int(start * n), int(end * n)])
                 else:
                     start_prev = segments[-1][0]
                     end_prev = segments[-1][1]
@@ -41,7 +42,7 @@ def find_segs(samples, threshold, min_dur, merge_dur,fs,n):
                         segments[-1][2] = end - start_prev + 1
                     else:
                         segments.append([start, end, dur])
-                        points.append([int(start*n), int(end*n)])
+                        points.append([int(start * n), int(end * n)])
 
             start = -1
             end = -1
@@ -68,10 +69,11 @@ def find_segs(samples, threshold, min_dur, merge_dur,fs,n):
 
     return points
 
+
 def calc_average(signal_filtered, n):
 
     average = []
-    average_all=[]
+    average_all = []
     for i in range(0, len(signal_filtered), n):
         average.append(np.mean(signal_filtered[i:i + n]))
 
@@ -79,10 +81,9 @@ def calc_average(signal_filtered, n):
         for j in range(n):
             average_all.append(average[i])
 
-    return average_all,average
+    return average_all, average
 
-    #return average
-
+    # return average
 
 
 # def test_cor():
@@ -190,73 +191,76 @@ def calc_average(signal_filtered, n):
 def test_cor():
 
     needle = np.loadtxt('needle2.txt').view(complex)
-    needle=needle[:8192]
-    needle=needle[::-1]
-    hay=np.loadtxt('hay2.txt').view(complex)
+    needle = needle[:8192]
+    needle = needle[::-1]
+    hay = np.loadtxt('hay2.txt').view(complex)
     print("Start")
-    needle_mn=np.mean(needle.real)+1j*np.mean(needle.imag)
-    #hay_mn=np.mean(hay.real)+1j*np.mean(hay.imag)
-    needle_new=(needle- needle_mn)/(np.sqrt(np.sum((needle- needle_mn)**2)))
+    needle_mn = np.mean(needle.real) + 1j * np.mean(needle.imag)
+    # hay_mn=np.mean(hay.real)+1j*np.mean(hay.imag)
+    needle_new = (needle - needle_mn) / \
+        (np.sqrt(np.sum((needle - needle_mn)**2)))
     #hay_new=(hay- hay_mn)/(np.sqrt(np.sum((hay- hay_mn)**2)))
-    #print(hay_new.shape)
-    fs=2e6
-    chunksize=2*int(fs)
+    # print(hay_new.shape)
+    fs = 2e6
+    chunksize = 2 * int(fs)
 
-    start=0
-    end=chunksize
-    points=[]
-    point_max=0
-    while start <= hay.shape[0]-2*int(fs):
+    start = 0
+    end = chunksize
+    points = []
+    point_max = 0
+    while start <= hay.shape[0] - 2 * int(fs):
         if(end > hay.shape[0]):
-            end=hay.shape[0]
-        print("{} {}".format(start,end))
+            end = hay.shape[0]
+        print("{} {}".format(start, end))
 
-        hay2=hay[start:end]
-        #print(hay[:5])
-        hay_mn=np.mean(hay2.real)+1j*np.mean(hay2.imag)
-        hay_new=(hay2- hay_mn)/(np.sqrt(np.sum((hay2- hay_mn)**2)))
+        hay2 = hay[start:end]
+        # print(hay[:5])
+        hay_mn = np.mean(hay2.real) + 1j * np.mean(hay2.imag)
+        hay_new = (hay2 - hay_mn) / (np.sqrt(np.sum((hay2 - hay_mn)**2)))
 
-        autocor=signal.fftconvolve(hay_new,needle_new,mode="full")
-        autocor_abs=np.absolute(autocor)
-        average_all,average=calc_average(autocor_abs,3*8192)
+        autocor = signal.fftconvolve(hay_new, needle_new, mode="full")
+        autocor_abs = np.absolute(autocor)
+        average_all, average = calc_average(autocor_abs, 3 * 8192)
 
         # print(np.average(average))
-        md=np.median(average)
-        st=np.std(average)
-        threshold=md+1.5*st
+        md = np.median(average)
+        st = np.std(average)
+        threshold = md + 1.5 * st
         # print(md)
         # print(st)
         print(threshold)
 
-        point=find_segs(average,threshold,20,5,int(fs),3*8192)
+        point = find_segs(average, threshold, 20, 5, int(fs), 3 * 8192)
         print(point)
         plt.subplot(211)
         plt.plot(autocor_abs)
-        #print(autocor_abs[:5])
+        # print(autocor_abs[:5])
         plt.plot(average_all)
 
         if(len(point) > 0):
-            initial=point[0][0]
-            final=point[0][1]
-            point_max=initial+np.argmax(autocor_abs[initial:final])
-            final_point=start+(point_max)
+            initial = point[0][0]
+            final = point[0][1]
+            point_max = initial + np.argmax(autocor_abs[initial:final])
+            final_point = start + (point_max)
             if(not final_point in points):
                 points.append(final_point)
-            print("Points {} {} {} {} {} {}".format(initial,final,start,point_max,autocor_abs[point_max],final_point))
-            plt.axvline(point_max,color='red')
+            print("Points {} {} {} {} {} {}".format(initial, final,
+                                                    start, point_max, autocor_abs[point_max], final_point))
+            plt.axvline(point_max, color='red')
 
         plt.subplot(212)
         plt.plot(average)
         plt.show()
-        start=start+int(fs)//2
-        end=start+chunksize
+        start = start + int(fs) // 2
+        end = start + chunksize
 
     print("printing points")
     print(points)
 
+
 def analysis(SignalInfo):
     is_peaks_all = False
-    filename = 'satellite_db (copy).json'
+    filename = 'satellite_db.json'
     bands = freqbands.getbands(SignalInfo, filename)
 
     chunksize = int(1024 * 2 * 2 * 2 * 2 * 2 * 2 * 2)
@@ -269,14 +273,14 @@ def analysis(SignalInfo):
         print(band, FLow, FHigh)
 
         signal_filtered = bandpass.filter_box(
-        SignalInfo, FLow, FHigh, chunksize)
+            SignalInfo, FLow, FHigh, chunksize)
 
         return signal_filtered
 
 
 def test_cor_double():
 
-    file2="/home/matrix/Desktop/SiqNAL/test/d6cd51d0002395bea758bda96d641518876bb922e8159b23b2704e9e_f145825000_d0_t1496600686.dat"
+    file2 = "/home/matrix/Desktop/SiqNAL/test/d6cd51d0002395bea758bda96d641518876bb922e8159b23b2704e9e_f145825000_d0_t1496600686.dat"
 
     SignalInfo2 = importfile.loadfile(file2)
 
@@ -285,70 +289,71 @@ def test_cor_double():
     else:
         SignalInfo2.filedata = read_wav.loaddata(SignalInfo2.filename)
 
-
-    hay=analysis(SignalInfo2)
-    hay=np.reshape(hay,(hay.shape[0],1))
+    hay = analysis(SignalInfo2)
+    hay = np.reshape(hay, (hay.shape[0], 1))
     print(hay.shape)
     needle = np.loadtxt('needle.txt').view(complex)
-    needle=needle[:8192]
-    needle=needle[::-1]
+    needle = needle[:8192]
+    needle = needle[::-1]
     print("Start")
-    needle_mn=np.mean(needle.real)+1j*np.mean(needle.imag)
-    needle_new=(needle- needle_mn)/(np.sqrt(np.sum((needle- needle_mn)**2)))
-    fs=2e6
-    chunksize=2*int(fs)
+    needle_mn = np.mean(needle.real) + 1j * np.mean(needle.imag)
+    needle_new = (needle - needle_mn) / \
+        (np.sqrt(np.sum((needle - needle_mn)**2)))
+    fs = 2e6
+    chunksize = 2 * int(fs)
 
-    start=0
-    end=chunksize
-    points=[]
-    point_max=0
-    i=0
-    while start <= hay.shape[0]-2*int(fs):
+    start = 0
+    end = chunksize
+    points = []
+    point_max = 0
+    i = 0
+    while start <= hay.shape[0] - 2 * int(fs):
         if(end > hay.shape[0]):
-            end=hay.shape[0]
-        print("{} {} {}".format(i,start,end))
-        i=i+1
+            end = hay.shape[0]
+        print("{} {} {}".format(i, start, end))
+        i = i + 1
 
-        hay2=hay[start:end]
-        #print(hay[:5])
-        hay_mn=np.mean(hay2.real)+1j*np.mean(hay2.imag)
-        hay_new=(hay2- hay_mn)/(np.sqrt(np.sum((hay2- hay_mn)**2)))
+        hay2 = hay[start:end]
+        # print(hay[:5])
+        hay_mn = np.mean(hay2.real) + 1j * np.mean(hay2.imag)
+        hay_new = (hay2 - hay_mn) / (np.sqrt(np.sum((hay2 - hay_mn)**2)))
 
-        autocor=signal.fftconvolve(hay_new,needle_new,mode="full")
-        autocor_abs=np.absolute(autocor)
-        average_all,average=calc_average(autocor_abs,3*8192)
-        md=np.median(average)
-        st=np.std(average)
-        threshold=md+1.5*st
-        point=find_segs(average,threshold,30,5,int(fs),3*8192)
+        autocor = signal.fftconvolve(hay_new, needle_new, mode="full")
+        autocor_abs = np.absolute(autocor)
+        average_all, average = calc_average(autocor_abs, 3 * 8192)
+        md = np.median(average)
+        st = np.std(average)
+        threshold = md + 1.5 * st
+        point = find_segs(average, threshold, 30, 5, int(fs), 3 * 8192)
 
         plt.subplot(211)
         plt.plot(autocor_abs)
-        #print(autocor_abs[:5])
+        # print(autocor_abs[:5])
         plt.plot(average_all)
 
         if(len(point) > 0):
-            initial=point[0][0]
-            final=point[0][1]
-            point_max=initial+np.argmax(autocor_abs[initial:final])
-            final_point=start+(point_max)
+            initial = point[0][0]
+            final = point[0][1]
+            point_max = initial + np.argmax(autocor_abs[initial:final])
+            final_point = start + (point_max)
             if(not final_point in points):
                 points.append(final_point)
-            print("Points {} {} {}".format(start,point_max,final_point))
-            plt.axvline(point_max,color='red')
+            print("Points {} {} {}".format(start, point_max, final_point))
+            plt.axvline(point_max, color='red')
 
         plt.subplot(212)
         plt.plot(average)
         plt.savefig(str(i) + ".png", dpi=1600)
         plt.clf()
-        #plt.show()
-        start=start+int(fs)//2
-        end=start+chunksize
+        # plt.show()
+        start = start + int(fs) // 2
+        end = start + chunksize
 
     print("printing points")
     print(points)
-    np.savetxt("Corr.csv",points)
+    np.savetxt("Corr.csv", points)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     test_cor_double()
-    #test_cor()
+    # test_cor()
