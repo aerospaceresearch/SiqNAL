bb835978358d2a903c701eb9a85874c50f09abea
Jay Krishna <jayk674@gmail.com>
2017-08-22 01:24:42 +0530

    Added documentation

--- Modules/freqbands.py
+++ Modules/freqbands.py
@@ -1,9 +1,37 @@
+"""
+    **Author :** *Jay Krishna*
+
+    This module reads the json database of satellites & correspondingly
+    populates list object for construction of bandpass filter and signal detection.
+
+"""
+
 import json
 import os
 from Modules import SignalData
 
 
 def getbands(SignalInfo, filename):
+    """
+        This function go through the json database of satellites, read the meta data
+        about them usch as name and transmission method used by them. It also
+        reads about their transponder data particularly Downlink frequency
+        along with width of frequency band used by them which is further corrected
+        for doppler shift and stored. 
+
+        Parameters
+        -----------------------
+            SignalInfo : object
+                Instance of class SignalData having meta-data of file and signal.
+            filename: str
+                Absolute path to json satellite database.
+
+        Returns
+        ------------------------------
+            bands : list
+                List object containing meta data about each satellite.
+
+    """
     bands = []
     flow = SignalInfo.Fcentre - SignalInfo.Fsample / 2
     fhigh = SignalInfo.Fcentre + SignalInfo.Fsample / 2
@@ -28,5 +56,4 @@ def getbands(SignalInfo, filename):
                 if(lower > flow and upper < fhigh):
                     bands.append((name, lower, upper, description))
 
-    # print(bands)
     return bands
