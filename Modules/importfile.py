54ae1ae5403ff231fa5a429bd1f29f47156d0554
Jay Krishna <jayk674@gmail.com>
2017-08-22 01:35:19 +0530

    Added documentation

--- Modules/importfile.py
+++ Modules/importfile.py
@@ -1,3 +1,10 @@
+"""
+    **Author :** *Jay Krishna*
+
+    This module reads the json file associated with a signal file for
+    server version of this project.
+
+"""
 import json
 import numpy as np
 import os
@@ -5,8 +12,23 @@ from os.path import splitext
 
 from Modules import SignalData
 
-
 def loadfile(filename):
+    """
+        This function read the json file associated with the signal file and extract 
+        centre frequency and sampling frequency of the signal file.
+        
+        Parameters
+        -----------------------
+            filename : string
+                Absolute path of signal file whose associated json file need to be read.
+
+        Returns
+        ------------------------------
+            SignalInfo : object
+                Instance of class SignalData having meta-data of file and signal.
+
+    """
+
     SignalMeta = SignalData.Signal()
     _, file_extension = os.path.splitext(filename)
     if file_extension == ".dat":
