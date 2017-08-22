537ccaae88f93ce7aee645f40b85ce66d8140521
Jay Krishna <jayk674@gmail.com>
2017-08-22 16:05:50 +0530

    Fixed typos

--- Track/aprs.py
+++ Track/aprs.py
@@ -1,20 +1,20 @@
 """
     **Author :** *Jay Krishna*
     
-    This module detects the presence of signal_chunk sent following Automatic Packet Reporting System(APRS). Instead of having
+    This module detects the presence of signal sent following Automatic Packet Reporting System(APRS). Instead of having
     constant threshold, dynamic thresholding is applied based upon leading & lagging cells.
 
     Approach
     ----------------------------
      
-    * signal_chunk is broken down in chunks of one second each.
-    * At each point of smaller signal_chunk threshold is calculated based upon difference of lagging & leading reference cells after removing guard cells from both sides.
-    * Point where APRS signal_chunk is suppossed to start is found & checked against peak induced by random noise.
+    * signal is broken down in chunks of one second each.
+    * At each point of smaller signal threshold is calculated based upon difference of lagging & leading reference cells after removing guard cells from both sides.
+    * Point where APRS signal is suppossed to start is found & checked against peak induced by random noise.
 
     Reference
     ------------------------------
     
-    `Barkat, Mourad & Varshney, P.K (1987). On adaptive cell-averaging CFAR (Constant False-Alarm Rate) radar signal_chunk detection. Final Technical Report, Jun. 1984 - Dec. 1986 Syracuse Univ., NY. Dept. of Electrical and Computer Engineering <https://goo.gl/15t64a>`_
+    `Barkat, Mourad & Varshney, P.K (1987). On adaptive cell-averaging CFAR (Constant False-Alarm Rate) radar signal detection. Final Technical Report, Jun. 1984 - Dec. 1986 Syracuse Univ., NY. Dept. of Electrical and Computer Engineering <https://goo.gl/15t64a>`_
 
 """
 
@@ -29,7 +29,7 @@ from Modules import SignalData
 def radar_detect(diff, refLength, threshold_min=1.2, threshold_ratio=10, threshold_max=1.5):
     """
         
-        Based upon the values calculated start of APRS signal_chunk is detected while taking care of false results
+        Based upon the values calculated start of APRS signal is detected while taking care of false results
         due to spikes induced by noise.
 
         Parameters
@@ -41,14 +41,14 @@ def radar_detect(diff, refLength, threshold_min=1.2, threshold_ratio=10, thresho
             threshold_min : float
                 Minimum level of threshold
             threshold_ratio : float
-                Minimum ratio of detected point and mean of signal_chunk chunk
+                Minimum ratio of detected point and mean of signal chunk
             threshold_max : float
                 Maximum value of local maxima to ensure false detection due to random spikes.
         
         Returns
         -------------------------
             point : int
-                Starting point of aprs signal_chunk if present.
+                Starting point of aprs signal if present.
                 
     """
     point = -1
@@ -77,7 +77,7 @@ def cfar(signal_chunk, refLength=10000, guardLength=10, p=0.001):
         Parameters
         -----------------------
             signal_chunk : ndarray
-                Numpy complex array of signal_chunk.
+                Numpy complex array of signal.
             refLength : int
                 Length of reference cells
             guardLength : int
@@ -88,7 +88,7 @@ def cfar(signal_chunk, refLength=10000, guardLength=10, p=0.001):
         Returns
         -----------------------
             point : int
-                Starting point of aprs signal_chunk if present.
+                Starting point of aprs signal if present.
 
     """
 
@@ -116,20 +116,20 @@ def cfar(signal_chunk, refLength=10000, guardLength=10, p=0.001):
 
 def check(SignaIInfo, signal_chunk):
     """
-        Breaks the signal_chunk into smaller chunks and send them
-        sequentially for APRS signal_chunk detection.
+        Breaks the signal into smaller chunks and send them
+        sequentially for APRS signal detection.
 
         Parameters
         -----------------------
-            signal_chunkInfo : object
-                Instance of class SignalData having meta-data of file and signal_chunk.
+            SignalInfo : object
+                Instance of class SignalData having meta-data of file and signal.
             signal_chunk : ndarray
-                Numpy complex array of filtered signal_chunk.
+                Numpy complex array of filtered signal.
 
         Returns
         ------------------------------
             points : list
-                List of starting index of aprs signal_chunk.
+                List of starting index of aprs signal.
 
     """
 
